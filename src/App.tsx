8b1b2262c814a1509101fc6f8a5ac9e8b02ee422
eMedicStore <eMedicStore@ALTs-MacBook-Pro-2.local>
2023-02-06 15:57:35 +0100

    adding files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/navbar';
+import Menubar from './components/menubar';
+import Main from './components/pageBody';
+import GlobalStyle from './styles/global';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Nav />
+      <Menubar />
+      <Main />
+    </>
   );
 }
 
---
+++ src/components/menubar/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+	Container,
+	Text, 
+	Wrapper, 
+	LeftText
+} from "./styles"
+import { Menu } from '@material-ui/icons';
+
+const Menubar:React.FC = () => {
+	return (
+		<Container>
+			<Wrapper>
+				<Wrapper>
+					<Menu/>
+					<Text>All</Text>
+				</Wrapper>
+
+				<Wrapper>
+					<Text>Today's Deals</Text>
+					<Text>Customer Service</Text>
+					<Text>Gift Cards</Text>
+					<Text>Sell</Text>
+					<Text>Registry</Text>
+				</Wrapper>
+			</Wrapper>
+
+			<Wrapper>
+				<LeftText>Amazon's response to COVID-19</LeftText>
+			</Wrapper>
+		</Container>
+	)
+}
+
+export default Menubar
